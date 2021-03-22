8ad5a1f5543736c7ff605a9bc6ffc3f9c65574fb
Marianna Askova <marianna.askova@gmail.com>
2021-03-22 13:43:22 -0500

    Radius

--- 00-Setup/00-Demo/lib/demo.rb
+++ 00-Setup/00-Demo/lib/demo.rb
@@ -1,3 +1,10 @@
 def circle_area(radius)
   # TODO: Implement the `circle_area` method
+  if radius.negative?
+ 	return 0
+  else 
+  	phi = 3.14
+  	area = phi * radius **2
+  	return area
+  end
 end
